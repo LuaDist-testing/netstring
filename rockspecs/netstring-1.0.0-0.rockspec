3f464efdffc0a469e72d179931b58a2bbfacc960
LunaCI <lunaci@luadist.org>
2018-06-08 02:21:21 +0000

    Update to version 1.0.1-0

--- rockspecs/netstring-1.0.0-0.rockspec
+++ rockspecs/netstring-1.0.0-0.rockspec
@@ -1,17 +1,9 @@
--- This file was automatically generated for the LuaDist project.
-
 package = "netstring"
 version = "1.0.0-0"
--- LuaDist source
 source = {
-  tag = "1.0.0-0",
-  url = "git://github.com/LuaDist-testing/netstring.git"
+    url = "git://github.com/jprjr/netstring.lua.git",
+    tag = "1.0.0"
 }
--- Original source
--- source = {
---     url = "git://github.com/jprjr/netstring.lua.git",
---     tag = "1.0.0"
--- }
 description = {
     summary = "Implementation of DBJ's netstring for lua",
     homepage = "https://github.com/jprjr/netstring.lua",
--- src/netstring.lua
+++ src/netstring.lua
@@ -1,4 +1,4 @@
--- Copyright 2016 John Regan <john@jrjrtech.com
+-- Copyright 2016 John Regan <john@jrjrtech.com>
 
 local string_sub = string.sub
 local string_len = string.len
@@ -79,9 +79,12 @@ end
 function M.encode(...)
     local r = ""
     local err = {}
+    local args = {...}
 
-    for i,a in ipairs{...} do
-        if(type(a) == 'table') then
+    for i=1, #args do
+        local a = args[i]
+        local ta = type(a)
+        if ta == 'table' then
             -- grab table keys and sort
             local keys = {}
             for k in pairs(a) do keys[#keys+1] = k end
@@ -93,7 +96,7 @@ function M.encode(...)
                     r = r .. _encode(a[k])
                 end
             end
-         else
+         elseif ta ~= 'nil' then
              r = r .. _encode(a)
          end
     end
